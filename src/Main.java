450ea17ddc5850766ef1e97519474c586fc36460
MOHAMMED SAYED <omarsayed@gmail.com>
2023-05-08 20:05:43 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Object Person01
+        Person Person01 = new Person("James Peterson", 34, 180, 73.5);
+        System.out.println("========================================");
+        System.out.println("Name = " + Person01.getName());
+        System.out.println("Age = " + Person01.getAge());
+        System.out.println("Height = " + Person01.getHeight() + " cm");
+        System.out.println("Weight = " + Person01.getWeight() + " lbs");
+        // Create object person2 Bob Thornley, 25, 171cm, 68.2lbs and print
+        Person Person02 = new Person("Bob thornley", 25, 171, 68.2);
+        System.out.println("Name = " + Person02.getName());
+        System.out.println("Age = " + Person02.getAge());
+        System.out.println("Height = " + Person02.getHeight() + " cm");
+        System.out.println("Weight = " + Person02.getWeight() + " lbs");
+        System.out.println("========================================");
+
+        System.out.println("After a year, their profile is ");
+        // Person01 grow older
+        Person01.growOlder();
+        System.out.println("==========================================");
+        System.out.println("Name = " + Person01.getName());
+        System.out.println("Age = " + Person01.getAge());
+        System.out.println("Height = " + Person01.getHeight() + " cm");
+        System.out.println("Weight = " + Person01.getWeight() + " lbs");
+        System.out.println("==========================================");
+
+        Person02.growOlder();
+        System.out.println("==========================================");
+        System.out.println("Name = " + Person02.getName());
+        System.out.println("Age = " + Person02.getAge());
+        System.out.println("Height = " + Person02.getHeight() + " cm");
+        System.out.println("Weight = " + Person02.getWeight() + " lbs");
+        System.out.println("==========================================");
+
+        }
+
     }
-}
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Person {
-}
+// Person Object Blueprint
+public class Person {
+    // Declare All attributes, encapsulate using private keyword
+    private String name;
+    private int age;
+    private int height;
+    private double weight;
+    // Method Constructors
+    public Person(String name, int age, int height, double weight) { // list all setter methods
+        setName(name);
+        setAge(age);
+        setHeight(height);
+        setWeight(weight);
+    }
+    // Setter method for name, age, height, weight, growOlder:  Always USE void, pass all parameters
+    public void setName (String name) {
+        this.name = name;
+    }
+    // Getter methods for name etc, return value, no parameter passing
+    public String getName() {
+        return name;
+    }
+    // Setter methods for age etc,
+    public void setAge (int age) {this.age = age;}
+    // Getter method for age, return value, no parameter passing
+    public int getAge() {return age;}
+    // Use shortcut cmd  + n (Alt + Insert: windows)
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public double getWeight() {
+        return weight;
+    }
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    // Behavioural Method Component
+    public void growOlder(){
+       setAge(getAge()+1); // Increase age by 1
+       setHeight(getHeight()-1); // Decrease height by 1
+       setWeight(getWeight()-0.5);// Decrease the weight  by 0.5
+        }
+    }
